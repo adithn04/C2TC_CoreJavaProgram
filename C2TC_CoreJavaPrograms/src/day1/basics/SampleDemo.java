4787b4ea8ea40f9b22c4c43bacbc28c2d9b0798c
Asus <Asus@Adithyan>
2025-07-21 12:21:39 +0530

    New line added

--- C2TC_CoreJavaPrograms/src/day1/basics/SampleDemo.java
+++ C2TC_CoreJavaPrograms/src/day1/basics/SampleDemo.java
@@ -5,7 +5,7 @@ public class SampleDemo {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Welcome to JFS..!");
-
+		System.out.println("NEW LINE");
 	}
 
 }
