3e51277567527e9ae49feff249c86b1a2378a6b1
Asus <Asus@Adithyan>
2025-07-23 19:35:52 +0530

    These code files where include the day 3 performance.

---
+++ C2TC_CoreJavaPrograms/src/DayThree/Encapsulation/Encapsulation.java
@@ -0,0 +1,16 @@
+package DayThree.Encapsulation;
+
+public class Encapsulation {
+
+	public static void main(String[] args) {
+		
+		ObjectCreation obj = new ObjectCreation();
+		obj.setSerialNum(101);
+		obj.setName("Adithyan");
+		obj.setAge(20);
+		System.out.println(obj);
+
+
+	}
+
+}
