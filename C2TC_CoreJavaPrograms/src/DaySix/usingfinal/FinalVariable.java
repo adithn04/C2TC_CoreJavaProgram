cccf529f8ee87185ddd4f8f36c573c35a961a483
Asus <Asus@Adithyan>
2025-07-25 15:40:45 +0530

    These files contain concepts of Day 5.

---
+++ C2TC_CoreJavaPrograms/src/DaySix/usingfinal/FinalVariable.java
@@ -0,0 +1,34 @@
+// Program to demonstrate final Variable
+package DaySix.usingfinal;
+
+public class FinalVariable {
+
+	//final int x; // final instance variable must be initialized
+
+	final int x = 100;
+
+	// Declare a static blank final variable.
+	final static int Y;
+
+	// Declare & initialize static final variable.
+	final static int Z = 10;
+
+	// instance method
+	void change() {
+		int x = 30; // final variables can't be reassigned
+		int Y = 200; // final static variables can't be reassigned
+	}
+
+	@Override
+	public String toString() {
+		return "FinalVariable [x=" + x + ", Y" + Y + "]";
+	}
+
+	// Declare a static block to initialize the final static variable.
+	static {
+		Y = 20;
+		int Z = 100; // Once initialized can't be reassigned
+		System.out.println("Value of Y: " + Y);
+	}
+
+}
